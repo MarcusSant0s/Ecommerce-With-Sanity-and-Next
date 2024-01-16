fd181899c124e7f85e4f47fb39c84bad776c649b
MarcusSant0s <marcus.pereira.paulo@gmail.com>
2024-01-16 18:06:06 -0300

    Setup FrontEnd Done

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,37 @@
-import React from 'react'
 
-const index = () => {
+import React from 'react';
+import {client} from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const index = ({products, bannerData}) => {
   return (
-    <section>
-      The banner goes here
+    <>
+     <HeroBanner/>
 
-    <div>index</div>
+    <div className='products-heading'>
+      <h2>Beset Selling Products</h2>
+      <p>Speakers of many variations</p>  
+    </div>
 
-and footer here
-    </section>
+    <div className='products-conteiner'>
+        {products?.map((product) => product.name)    }
+    </div>
+
+    <FooterBanner/>
+    </>
     
   )
+}
+export const getServerSideProps = async () =>{
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return{
+    props: {products, bannerData}
+  }
 }
 
 export default index
