d8105c2696f861f0a963a567dda332ab8b718655
MarcusSant0s <marcus.pereira.paulo@gmail.com>
2024-01-17 20:46:31 -0300

    Error With Layout

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const index = ({products, bannerData}) => {
     </div>
 
     <div className='products-container'>
-        {products?.map((product) => <Product key={product.id} product={product}/>)    }
+        {products?.map((product) => <Product key={product._id} product={product}/>)    }
     </div>
 
     <FooterBanner footerBanner={bannerData && bannerData[0]}/>
