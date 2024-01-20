29df8700604070ebd286a92927c46330ad83b4a5
MarcusSant0s <marcus.pereira.paulo@gmail.com>
2024-01-20 11:43:44 -0300

    Front-end almost Done

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,39 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
-import {client, urlFor} from '../../lib/client'
+import { client, urlFor } from '../../lib/client';
+import { Product } from '../../components';
 
-const ProductDetails = ({ product, products}) => {
-  const {image, name, details, price} = product;
-
+const ProductDetails = ({ product, products }) => {
+  const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
   return (
     <div>
       <div className='product-detail-container' >
         <div>
           <div className='image-container'>
 
-          <img src={urlFor(image && image[0])} />  
+            <img src={urlFor(image && image[index])} className='product-detail-image'/>
           </div>
-{/*          <div
+                   <div
           className='small-images-container'>
-            {image?.map((item, i) =>{
+            {image?.map((item, i) =>(
               <img
               src={urlFor(item)}
-              className=''
-              onMouseEnter=''/>
-            })}
+              className={i === index ? 'small-image selected-image' : 'small-image'}
+              onMouseEnter={()=>setIndex(i)} key={i}/>
+           ))}
           </div> 
-          */}
+         
         </div>
 
-          <div className='product-details-desc'>
-            <h1>{name}</h1>
-            <div className='reviews'>
-            <AiFillStar/>
-            <AiFillStar/>
-            <AiFillStar/>
-            <AiFillStar/>
-              <AiOutlineMinus/>
+        <div className='product-detail-desc'>
+          <h1>{name}</h1>
+          <div className='reviews'>
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineMinus />
             </div>
+
             <p>
               (20)
             </p>
@@ -47,52 +50,81 @@ const ProductDetails = ({ product, products}) => {
             </h3>
             <p className='quantity-desc'>
               <span className='minus'
-              onClick=''>
-                <AiOutlineMinus/>
+                onClick=''>
+                <AiOutlineMinus />
+              </span>
+              <span className='num'
+                onClick=''>
+                0
+              </span>
+              <span className='pllus'
+                onClick=''>
+                <AiOutlinePlus />
               </span>
             </p>
-
           </div>
+          <div className='buttons'>
+            <button type='button' className='add-to-cart' onClick="">
+              Add to Card
+            </button>
+            <button type='button' className='buy-now' onClick="">
+              Buy Now
+            </button>
+          </div>
+
+        </div>
+      </div>
+
+
+      <div className='maylike-products-wrapper'>
+        <h2> You may also Like</h2>
+        <div className='marquee'>
+          <div className='maylike-products-container track'>
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
 
-export const getStaticPaths = async() => {
+export const getStaticPaths = async () => {
   const query = `*[_type == "product"]{
     slug {
       current
     }
   }
   `
-;
+    ;
 
   const products = await client.fetch(query);
 
   const paths = products.map((product) => ({
-    params:{
-      slug:product.slug.current
+    params: {
+      slug: product.slug.current
     }
   }));
 
   return {
-    paths, 
+    paths,
     fallback: 'blocking'
   }
 }
 
-export const getStaticProps = async ({ params: {slug} }) =>{
+export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
   const productsQuery = `*[_type == "product" ]`;
-  
+
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
 
-  
 
-  return{
-    props: {product, products}
+
+  return {
+    props: { product, products }
   }
 }
 
