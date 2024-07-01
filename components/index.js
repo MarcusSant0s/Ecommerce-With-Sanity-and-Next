cb394743828c2f6ec1465c3b05189b0fded4d03e
marcus.pereira.paulo@gmail.com <marcus.pereira.paulo@gmail.com>
2024-07-01 18:54:39 -0300

    --amend

--- components/index.js
+++ components/index.js
@@ -6,4 +6,3 @@ export {default as Layout} from './Layout';
 export {default as Navbar} from './Navbar';
 export {default as Product} from './Product';
 export {default as ProductsHandle} from './ProductsHandle'; 
-export {default as LottieAnimation} from './LottieAnimation';
