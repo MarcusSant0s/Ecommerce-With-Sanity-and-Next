bf1a85aa5a125ed88a80deae4b035061b9a8c4f9
marcus.pereira.paulo@gmail.com <marcus.pereira.paulo@gmail.com>
2024-12-22 15:20:27 -0300

    fixing production

--- components/Form/InsertForm.jsx
+++ components/Form/InsertForm.jsx
@@ -1,6 +1,6 @@
+'use client'
 
-  import React, { useEffect, useState } from "react";
-  import image from "next/image";
+  import React, { useEffect, useState } from "react"; 
   import { client, urlFor  } from "@/lib/client"; 
 import Image from "next/image";
 
